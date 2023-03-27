68e84fbaf44f12e8ff74ca166aeee3c8708b9ccf
Nathan Barros Miranda <113999531+NbMiranda@users.noreply.github.com>
2023-03-27 14:25:53 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,10 +8,12 @@ cd laravel
 docker-compose up -d
 echo ${C}  "Docker containers launched."${N}
 echo ${C}  "Building app inside containers."${N}
-docker-compose exec -T laravel composer install
+
 docker-compose exec -T laravel php artisan module:use
 docker-compose exec -T laravel php artisan module:enable
 docker-compose exec -T laravel php artisan optimize
 docker-compose exec -T laravel php artisan migrate:fresh
 docker-compose exec -T laravel php artisan db:seed
-echo ${C}  "App is ready at localhost:8080." ${N}
+echo ${C}  "Vue App is ready at localhost:8080." ${N}
+echo ${C}  "Laravel API ready at localhost:80." ${N}
+echo ${C}  "Thanks :) by Nate" ${N}
